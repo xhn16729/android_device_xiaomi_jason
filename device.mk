71bbcb7b2a374d29bdde995fac7b0eb442f0f46b
xhn16729 <xiong_haonan@163.com>
2018-12-23 06:32:33 +0000

    Use Google Camera as stock camera
    Change-Id: I3084844f895a09664d2fe35bde23c6de015b2e7d

--- device.mk
+++ device.mk
@@ -204,7 +204,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
+    GoogleCamera \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
